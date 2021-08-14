2593a7fd8a6cd7386d8ebe51206d4bc368d8eeb6
jiyansh <jiyansh1414@gmail.com>
2021-08-13 23:50:25 -0400

    Refactored some of the code.

---
+++ Leaky Faucet/Hot-Fire-Tests/Leaky_v1_HOTFIRE_6-25/Countdown.h
@@ -0,0 +1,66 @@
+#pragma once
+
+class Countdown {
+public:
+	Countdown(int count);
+	void Reset(int count);
+	void Update();
+
+	void Start();
+	void Stop();
+	bool IsDone() const;
+
+private:
+	int count;
+	bool running;
+	unsigned long previous_millis;
+};
+
+Countdown::Countdown(int count) : count(count) {
+	running = false;
+	previous_millis = 0;
+}
+
+void Countdown::Reset(int count) {
+	this->count = count;
+	running = false;
+	previous_millis = 0;
+}
+
+void Countdown::Update() {
+	if (running && count > 0) {
+		unsigned long current_millis = millis();
+
+		if (current_millis - previous_millis > 999) {
+			count--;
+			previous_millis = current_millis;
+		}
+
+		if (count == 35) {
+			digitalWrite(MPV, LOW);
+			Serial.println(" MPV: ENERGIZED ");
+		}
+		else if (count == 30) {
+			digitalWrite(NMV, LOW);
+			Serial.println(" NMV: ENERGIZED ");
+		}
+		else if (count == 5) {
+			digitalWrite(WD1, LOW);
+			//digitalWrite(WD2, LOW);
+			Serial.println(" WATER DELUGE ACTIVATED ");
+		}
+	}
+}
+
+void Countdown::Start() {
+	running = true;
+	previous_millis = millis();
+}
+
+void Countdown::Stop() {
+	running = false;
+}
+
+bool Countdown::IsDone() const {
+	return count == 0 && running;
+}
