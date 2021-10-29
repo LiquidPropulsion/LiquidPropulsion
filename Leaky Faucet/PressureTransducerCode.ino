fa0232986204e6e73b55a4349b2db723052c3b82
jiyansh <jiyansh1414@gmail.com>
2021-10-29 18:16:49 -0400

    Added pressure transducer tranformation values.

--- Leaky Faucet/PressureTransducerCode.ino
+++ Leaky Faucet/PressureTransducerCode.ino
@@ -1,10 +1,10 @@
-const int numTransducers = 7;
-int pressurePins[numTransducers] = {A0, A1, A2, A3, A4, A5, A6};
+const int numTransducers = 8;
+int pressurePins[numTransducers] = {A0, A1, A2, A3, A4, A5, A6, A7};
 
 /*The way this works is that the array stores the max pressure reading of each transducer.This corresponds to a reading of 1023 from analogRead().
 * So, when we get the raw value, we multiply by max / 1023 to get the value in psi.
 */
-float pressureMax[numTransducers] = {100, 200, 300, 400, 500, 600, 700};
+float pressureMax[numTransducers] = {0/1023, 500/1023, 500/1023, 0/1023, 3000/1023, 0/1023, 3000/1023};
 
 void setup() {
   Serial.begin(115200);
@@ -12,8 +12,14 @@ void setup() {
 
 void loop() {
   for (int i = 0; i < numTransducers; i++) {
-    Serial.print(pressureMax[i] * analogRead(pressurePins[i]) / 1023);
-    Serial.print(', ');
+      if (i == 7) {
+          float pressure = 3000 * (5 * read / 1023 - 0.5) / 5;
+          Serial.print(pressure);
+      }
+      else {
+          Serial.print(pressureMax[i] * analogRead(pressurePins[i]));
+          Serial.print(', ');
+      }
   }
   Serial.println();
 }
