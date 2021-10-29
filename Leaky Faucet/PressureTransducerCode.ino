25f6587b2c6136e1bab65aba94f82d15df56d1a7
jiyansh <jiyansh1414@gmail.com>
2021-10-29 04:17:06 -0400

    Added a transformation to display so that pressure is displayed in psi.

--- Leaky Faucet/PressureTransducerCode.ino
+++ Leaky Faucet/PressureTransducerCode.ino
@@ -1,14 +1,19 @@
-int numDucers = 7;
-int pressurePins[] = {A0, A1, A2, A3, A4, A5, A6};
+const int numTransducers = 7;
+int pressurePins[numTransducers] = {A0, A1, A2, A3, A4, A5, A6};
+
+/*The way this works is that the array stores the max pressure reading of each transducer.This corresponds to a reading of 1023 from analogRead().
+* So, when we get the raw value, we multiply by max / 1023 to get the value in psi.
+*/
+float pressureMax[numTransducers] = {100, 200, 300, 400, 500, 600, 700};
 
 void setup() {
   Serial.begin(115200);
 }
 
 void loop() {
-  for (int i = 0; i < numDucers; i++) {
-    Serial.print(analogRead(pressurePins[i]));
-    Serial.print(',');
+  for (int i = 0; i < numTransducers; i++) {
+    Serial.print(pressureMax[i] * analogRead(pressurePins[i]) / 1023);
+    Serial.print(', ');
   }
   Serial.println();
 }
